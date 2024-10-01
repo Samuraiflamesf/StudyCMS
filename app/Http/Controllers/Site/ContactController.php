65678532ed619938f3a86e03a8d053d564e58ce6
samuraiflamesf <samuraiflamesf@gmail.com>
2024-09-30 21:31:19 -0300

    Add toastr, Request para form do Contato e validacao dos envios

--- app/Http/Controllers/Site/ContactController.php
+++ app/Http/Controllers/Site/ContactController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers\Site;
 use App\Models\Contact;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ContactFormRequest;
 use App\Notifications\NewContact;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Notification;
 
 
@@ -17,16 +17,15 @@ class ContactController extends Controller
         return view('site.contact.index');
     }
 
-    public function form(Request $request)
+    public function form(ContactFormRequest $request)
     {
         // Criação do contato
         $contact = Contact::create($request->all());
 
         // Notificar o usuário com a nova instância de contato
         Notification::route('mail', config('mail.from.address'))->notify(new NewContact($contact));
-
-        dump($request->all());
-
-        return response()->json(['message' => 'Mensagem enviada com sucesso!']);
+        // Display a success toast with no title
+        toastr()->success('O contato foi enviado com sucesso!');
+        return back();
     }
 }
---
+++ app/Http/Requests/ContactFormRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ContactFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required|',
+        ];
+    }
+    public function messages(){
+        return [
+            'name.required' => 'O campo nome é obrigatório.',
+            'email.required'=> 'O campo e-mail é obrigatório.',
+            'email.email' => 'Digite um e-mail válido.',
+            'message.required'=> 'O campo mensagem é obrigatório.'
+            ];
+    }
+}
